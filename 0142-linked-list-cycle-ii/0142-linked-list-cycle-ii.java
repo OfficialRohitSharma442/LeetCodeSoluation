413310cf792472ab62bcee7cbbabaedb203375b1
Rohit Sharma <71854995+OfficialRohitSharma442@users.noreply.github.com>
2022-10-16 18:03:50 +0530

    Time: 1 ms (74.29%), Space: 45.2 MB (44.45%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,16 +1,21 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        //this is simple fast slow aproch to find cycle in linklist
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) break;
+            // if slow == fast break loop 
         }
         if (fast == null || fast.next == null) return null;
+        //if any node is null it menes no cycle in linklist
         while (head != slow) {
+            // after breaking we forword head whith slow if slow is equal to head then found the node where cycle begain
             head = head.next;
             slow = slow.next;
         }
-        return head;
+        
+        return slow;
     }
 }
